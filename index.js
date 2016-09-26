118e453cba7c889efc4c5be98c4d91796851a2ec
Nicole <nicole_agurcia@unitec.edu>
2016-09-26 14:05:54 -0600

    index.js

--- index.js
+++ index.js
@@ -30,9 +30,15 @@ app.get('*', function(req, res) {
 });*/
 io.sockets.on('connection', socket);
 
-
+/*
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
+
+*/
+app.set('port', (process.env.PORT || 3000));
+http.listen(app.get('port'), function() {
+	console.log('listening on port: ' + app.get('port'));
+});
 
